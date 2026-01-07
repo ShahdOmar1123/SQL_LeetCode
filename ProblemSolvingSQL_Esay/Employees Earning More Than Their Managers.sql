df94d4a8ca69dd61e3b445853450dde8ad2aed64
shahd omar <shahdomar444466@gmail.com>
2026-01-07 23:31:23 +0200

    Add files via upload

---
+++ ProblemSolvingSQL_Esay/Employees Earning More Than Their Managers.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+#https://leetcode.com/problems/employees-earning-more-than-their-managers/
+SELECT
+    e.name AS Employee
+FROM Employee e
+JOIN Employee m
+    ON e.managerId = m.id
+WHERE e.salary > m.salary;
